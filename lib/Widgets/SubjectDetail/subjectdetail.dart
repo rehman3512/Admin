72ef72d7237c85ec109ea4747d58fe5396ecefe7
REHMAN KHAN <rehmankh2256@gmail.com>
2025-11-02 12:08:46 -0800

    1

--- lib/Widgets/SubjectDetail/subjectdetail.dart
+++ lib/Widgets/SubjectDetail/subjectdetail.dart
@@ -10,6 +10,7 @@ import 'package:admin/Widgets/TextWidget/textwidget.dart';
 import 'package:admin/routes/approutes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SubjectDetail extends StatelessWidget {
   final SubjectModel subject;
@@ -28,33 +29,77 @@ class SubjectDetail extends StatelessWidget {
     adminController.classController.text = subject.classTime;
 
     return Scaffold(
+      // appBar: AppBar(
+      //   backgroundColor: AppColors.purpleColor,
+      //   leading: IconButton(
+      //     onPressed: () {
+      //       Get.back();
+      //     },
+      //     icon: Icon(
+      //       Icons.arrow_back_ios_new_outlined,
+      //       color: AppColors.whiteColor,
+      //     ),
+      //   ),
+      //   title: TextWidget.h2(subject.subject, AppColors.whiteColor, context),
+      // ),
+
       appBar: AppBar(
         backgroundColor: AppColors.purpleColor,
+        elevation: 0,
         leading: IconButton(
-          onPressed: () {
-            Get.back();
-          },
-          icon: Icon(
-            Icons.arrow_back_ios_new_outlined,
+       onPressed: () => Get.back(),
+          icon: Icon(Icons.arrow_back_ios_new_rounded, color: AppColors.whiteColor, size: 20),
+        ),
+        title: Text(
+          subject.subject,
+          style: GoogleFonts.poppins(
+            fontSize: 19,
+            fontWeight: FontWeight.w700,
             color: AppColors.whiteColor,
+            letterSpacing: 0.5,
           ),
         ),
-        title: TextWidget.h2(subject.subject, AppColors.whiteColor, context),
+        centerTitle: false,
+        actions: [
+          // REQUESTS ICON
+          _iconOnlyButton(
+            icon: Icons.pending_actions_rounded,
+            color: AppColors.neonPurple,
+            onTap: () => Get.toNamed(AppRoutes.enrollmentRequests, arguments: subject),
+          ),
+          const SizedBox(width: 8),
+          // STUDENTS ICON
+          _iconOnlyButton(
+            icon: Icons.people_alt_rounded,
+            color: AppColors.neonGreen,
+            onTap: () => Get.to(() => EnrolledStudents(subjectId: subject.id)),
+          ),
+          const SizedBox(width: 16),
+        ],
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [AppColors.purpleColor, AppColors.purpleColor.withOpacity(0.9)],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+        ),
       ),
       backgroundColor: AppColors.whiteColor,
-      floatingActionButton: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-        FloatingActionButton(onPressed: (){
-          Get.toNamed(AppRoutes.enrollmentRequests,arguments: subject);
-        },child: Icon(Icons.ac_unit_outlined),),
-        SizedBox(height: 20,),
-        FloatingActionButton(onPressed: (){
-          // Get.toNamed(AppRoutes.enrolledStudents);
-          Get.to(() => EnrolledStudents(subjectId: subject.id));
-        },child: Icon(Icons.read_more),)
-      ],),
+      // floatingActionButton: Column(
+      //   crossAxisAlignment: CrossAxisAlignment.end,
+      //   mainAxisAlignment: MainAxisAlignment.end,
+      //   children: [
+      //   FloatingActionButton(onPressed: (){
+      //     Get.toNamed(AppRoutes.enrollmentRequests,arguments: subject);
+      //   },child: Icon(Icons.ac_unit_outlined),),
+      //   SizedBox(height: 20,),
+      //   FloatingActionButton(onPressed: (){
+      //     // Get.toNamed(AppRoutes.enrolledStudents);
+      //     Get.to(() => EnrolledStudents(subjectId: subject.id));
+      //   },child: Icon(Icons.read_more),)
+      // ],),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Padding(
@@ -136,4 +181,32 @@ class SubjectDetail extends StatelessWidget {
       ),
     );
   }
+}
+
+
+Widget _iconOnlyButton({
+  required IconData icon,
+  required Color color,
+  required VoidCallback onTap,
+}) {
+  return GestureDetector(
+    onTap: onTap,
+    child: AnimatedContainer(
+      duration: const Duration(milliseconds: 200),
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.white.withOpacity(0.15),
+        border: Border.all(color: color.withOpacity(0.6), width: 1.5),
+        boxShadow: [
+          BoxShadow(
+            color: color.withOpacity(0.5),
+            blurRadius: 12,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Icon(icon, size: 20, color: Colors.white),
+    ),
+  );
 }
